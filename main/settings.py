303b3c0527acbc5e42698d507541b8927e76f909
jchaveztorrez <jchaveztorrez91@gmail.com>
2025-06-13 13:20:11 -0400

    configuracion

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # Hosts permitidos
 ALLOWED_HOSTS = ['*'] if DEBUG else ['themause.onrender.com']
-ALLOWED_HOSTS = ['backenddj-1qj9.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 DEBUG = True
